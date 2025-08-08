0f24eff2cd28682719615733ad1b3a2409d9928b
DenisDedinkin123 <dedinkinjob@gmail.com>
2025-08-08 16:28:50 +0300

    lesson1

---
+++ test/lesson2_if.py
@@ -0,0 +1,9 @@
+password = '12345'
+
+if (password == '12345'):
+    print("passed")
+
+else:
+    print( "неверно")
+
+print("final")
---
+++ test/lesson2_operators.py
@@ -0,0 +1,21 @@
+# user_login = "adam"
+#user_pass = "Qwerty"
+
+#login = input("Login:")
+#password = input("Password:")
+
+#if login == user_login and password == user_pass:
+
+ #   print("secret is open")
+#else:
+ #   print("locked")
+
+crit1 = "red"
+crit2 = "lock"
+
+color = input("Color:")
+feature = input("Feature:")
+if color == crit1 and feature == crit2:
+    print("Buy it!")
+else:
+    print("похожу,посмотрю")

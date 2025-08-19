19d18e2c8860681a32e30c324d58f4567d34ef01
DenisDedinkin123 <dedinkinjob@gmail.com>
2025-08-19 17:12:37 +0300

    lesson1

--- lesson3/lesson_3_task_1.py
+++ lesson3/lesson_3_task_1.py
@@ -4,3 +4,15 @@ my_user = User('Денис', 'Петров')
 
 print(my_user.first_name)
 print(my_user.last_name)
+
+
+def print_first_name(self):
+    print(self.first_name)
+
+
+def print_last_name(self):
+    print(self.last_name)
+
+
+def print_full_name(self):
+    print(self.first_name, self.last_name)

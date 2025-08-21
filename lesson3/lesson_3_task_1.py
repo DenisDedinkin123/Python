7648a212f7f3cd60079adaf648d10c1d5cd3f06d
DenisDedinkin123 <dedinkinjob@gmail.com>
2025-08-21 16:11:18 +0300

    lesson1

--- lesson3/lesson_3_task_1.py
+++ lesson3/lesson_3_task_1.py
@@ -2,17 +2,6 @@ from user import User
 
 my_user = User('Денис', 'Петров')
 
-print(my_user.first_name)
-print(my_user.last_name)
-
-
-def print_first_name(self):
-    print(self.first_name)
-
-
-def print_last_name(self):
-    print(self.last_name)
-
-
-def print_full_name(self):
-    print(self.first_name, self.last_name)
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()

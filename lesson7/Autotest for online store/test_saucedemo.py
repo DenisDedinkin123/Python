382fc7d2518c59beb5a17b41b4a210ad637be6a2
DenisDedinkin123 <dedinkinjob@gmail.com>
2025-09-22 22:49:33 +0300

    lesson1

--- lesson7/Autotest for online store/test_saucedemo.py
+++ lesson7/Autotest for online store/test_saucedemo.py
@@ -11,8 +11,8 @@ from checkout_overview_page import CheckoutOverviewPage
 # Данные для теста
 USERNAME = "standard_user"
 PASSWORD = "secret_sauce"
-FIRST_NAME = "Sergey"
-LAST_NAME = "Latynin"
+FIRST_NAME = "Denis"
+LAST_NAME = "Dedinkin"
 ZIP_CODE = "302023"
 EXPECTED_TOTAL = 58.29  # числовое значение
 ITEMS_TO_ADD = [
